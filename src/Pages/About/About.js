99def6ea21b6e5660b44f539420066e5fbbb2e8f
Ashraful <ashrafulislam2880@gmail.com>
2022-01-27 12:52:21 +0600

    About page Design Complete

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,100 +1,87 @@
 import React from 'react';
+import aboutImage from '../../Images/about us.png';
+import mdAboutImage from '../../Images/md-about us.png';
 
 const About = () => {
     return (
-        
-        <div class="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
-            <div class="flex lg:flex-row flex-col lg:gap-8 sm:gap-10 gap-12">
-                <div class="w-full lg:w-6/12 text-left">
-                    <h2 class="w-full font-bold lg:text-4xl text-3xl lg:leading-10 leading-9">
-                        We are here to make great design accessible and delightfull for everyone
+        <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+            <div className="flex lg:flex-row flex-col lg:gap-8 sm:gap-10 gap-12">
+                <div className="w-full lg:w-6/12 text-left">
+                    <h2 className="w-full font-bold lg:text-4xl text-3xl lg:leading-10 leading-9">
+                        We are here to make a better Journey for everyone. Lets go travel with us.
                     </h2>
-                    <p class="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire,</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire,</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire,</p>
                 </div>
-                <div class="w-full lg:w-6/12">
-                    <img class="lg:block hidden w-full" src="https://i.ibb.co/RjNH7QB/Rectangle-122-1.png" alt="people discussing on board" />
-                    <img class="lg:hidden sm:block hidden w-full" src="https://i.ibb.co/16fPqrg/Rectangle-122-2.png" alt="people discussing on board" />
-                    <img class="sm:hidden block w-full" src="https://i.ibb.co/Jxhpxh6/Rectangle-122.png" alt="people discussing on board" />
+                <div className="w-full lg:w-6/12">
+                    <img className="lg:block hidden w-full" src={aboutImage} alt="people discussing on board" />
+                    <img className="lg:hidden sm:block hidden w-full" src={mdAboutImage} alt="people discussing on board" />
+                    <img className="sm:hidden block w-full" src={mdAboutImage} alt="people discussing on board" />
                 </div>
             </div>
-
-            <div class="relative mt-24">
-                <div class="grid sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4">
-                    <div class="z-20 w-12 h-12 bg-gray-800 rounded-full flex justify-center items-center">
-                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/about-us-3-svg1.svg" alt="flag" />
-                    </div>
-
-                  <img class="z-20" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/about-us-3-svg2.svg" alt="note" />
-
-                   <img class="z-20 sm:block hidden" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/about-us-3-svg3.svg" alt="users" />
-                </div>
-                <hr class="z-10 absolute top-2/4 w-full bg-gray-200" />
-            </div>
-            <div class="grid sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4">
+            <div className="grid sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4 mt-12">
                 <div>
-                    <p class="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800 mt-6">Founded</p>
-                    <p class="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <i className="fas fa-car-side text-2xl text-indigo-700 ring-2 ring-indigo-700 p-2 rounded-full hover:bg-indigo-700 hover:text-white"></i>
+                    <p className="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800 mt-6">WE OFFER DAILY TOURS</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 mt-6 mb-4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                 </div>
                 <div>
-                    <p class="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800 mt-6">50M montly enrichments</p>
-                    <p class="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <i className="fas fa-plane-departure text-2xl text-indigo-700 ring-2 ring-indigo-700 p-2 rounded-full hover:bg-indigo-700 hover:text-white"></i>
+                    <p className="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800 mt-6">NEW DESTINATIONS</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                 </div>
-                <div class="sm:block hidden">
-                    <p class="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800 mt-6">400k User</p>
-                    <p class="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                <div className="sm:block hidden">
+                    <i className="far fa-handshake text-2xl text-indigo-700 ring-2 ring-indigo-700 p-2 rounded-full hover:bg-indigo-700 hover:text-white"></i>
+                    <p className="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800 mt-4">400k Satisfied Customer</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                 </div>
             </div>
-            <div class="sm:hidden block relative mt-8">
-                <div class="grid sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4">
-                   <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/about-us-3-svg3.svg" alt="user" />
-                </div>
-                <hr class="z-10 absolute top-2/4 w-full bg-gray-200" />
-            </div>
-            <div class="sm:hidden grid sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4">
+            <div className="sm:hidden grid sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4">
                 <div>
-                    <p class="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800 mt-6">400k User</p>
-                    <p class="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <i className="far fa-handshake text-2xl text-indigo-700 ring-2 ring-indigo-700 p-2 rounded-full hover:bg-indigo-700 hover:text-white"></i>
+                    <p className="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800 mt-6">400k Satisfied Customer</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 mt-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                 </div>
             </div>
 
-            <div class="flex lg:flex-row flex-col md:gap-14 gap-16 justify-between lg:mt-20 mt-16">
-                <div class="w-full lg:w-6/12 text-left">
-                    <h2 class="font-bold lg:text-4xl text-3xl lg:leading-9 leading-7 text-gray-800">Our Mission</h2>
-                    <p class="font-normal text-base leading-6 text-gray-600 mt-6 w-full lg:w-10/12 xl:w-9/12">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
-                    <p class="font-normal text-base leading-6 text-gray-600 w-full lg:w-10/12 xl:w-9/12 mt-10">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+            <div className="flex lg:flex-row flex-col md:gap-14 gap-16 justify-between lg:mt-20 mt-16">
+                <div className="w-full lg:w-6/12 text-left">
+                    <h2 className="font-bold lg:text-4xl text-3xl lg:leading-9 leading-7 text-gray-800">Our Mission</h2>
+                    <p className="font-normal text-base leading-6 text-gray-600 mt-6 w-full lg:w-10/12 xl:w-9/12">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 w-full lg:w-10/12 xl:w-9/12 mt-10">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
                 </div>
-                <div class="w-full lg:w-6/12">
-                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 lg:gap-12 gap-10">
+                <div className="w-full lg:w-6/12">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 lg:gap-12 gap-10">
                         
-                        <div class="flex p-4 shadow-md">
-                            <div class="mr-6">
-                               <img class="mr-6"  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/about-us-3-svg4.svg" alt="team card" />
+                        <div className="flex p-4 shadow-md">
+                            <div className="mr-6 text-2xl mt-4 text-indigo-600">
+                                <i className="far fa-handshake"></i>
                             </div>
-                            <div class="">
-                                <p class="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800">Team</p>
-                                <p class="mt-2 font-normal text-base leading-6 text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <div className="">
+                                <p className="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800">Team</p>
+                                <p className="mt-2 font-normal text-base leading-6 text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                             </div>
                         </div>
 
                         
-                        <div class="flex p-4 shadow-md">
-                            <div class="mr-6">
-                               <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/about-us-3-svg5.svg" alt="board card" />
+                        <div className="flex p-4 shadow-md">
+                            <div className="mr-6">
+                                <i className="fas fa-people-arrows text-2xl text-indigo-600"></i>
                             </div>
-                            <div class="">
-                                <p class="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800">Board</p>
-                                <p class="mt-2 font-normal text-base leading-6 text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <div className="">
+                                <p className="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800">Board</p>
+                                <p className="mt-2 font-normal text-base leading-6 text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                             </div>
                         </div>
 
                         
-                        <div class="flex p-4 shadow-md">
-                            <div class="mr-6">
-                               <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/about-us-3-svg6.svg" alt="press card" />
+                        <div className="flex p-4 shadow-md">
+                            <div className="mr-6 mt-6">
+                                <i className="far fa-envelope text-2xl text-indigo-600"></i>
                             </div>
-                            <div class="">
-                                <p class="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800">Press</p>
-                                <p class="mt-2 font-normal text-base leading-6 text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <div className="">
+                                <p className="font-semibold lg:text-2xl text-xl lg:leading-6 leading-5 text-gray-800">Press</p>
+                                <p className="mt-2 font-normal text-base leading-6 text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                             </div>
                         </div>
                     </div>
