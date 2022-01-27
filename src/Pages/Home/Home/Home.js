f99ff6d11e3f1d6fcabeb2a68afc351ae1ae9a3e
Ashraful <ashrafulislam2880@gmail.com>
2022-01-27 21:45:23 +0600

    Blog Page Added in Home Page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Blogs from '../../Blogs/Blogs';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Blogs/>
         </div>
     );
 };
