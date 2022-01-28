29321fee50890bf4f15a7e9ea1ecab97af5c91dd
Ashraful <ashrafulislam2880@gmail.com>
2022-01-28 18:34:04 +0600

    Service Page Added

--- src/Pages/Dashboard/DashboardHome/DashboardHome.js
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.js
@@ -3,20 +3,20 @@ import useAuth from '../../../Hooks/UseAuth';
 
 const DashboardHome = () => {
     const {user}=useAuth();
+    
     return (
-        
-        <div class="lg:px-6 2xl:px-0">
-            <div class="mx-auto container relative z-0 px-4 xl:px-0">
-                <div class="flex flex-col-reverse md:flex-row">
-                    <div class="md:w-3/5 md:pt-24 pb-10 lg:py-32 xl:py-48">
+        <div className="lg:px-6 2xl:px-0">
+            <div className="mx-auto container relative z-0 px-4 xl:px-0">
+                <div className="flex flex-col-reverse md:flex-row">
+                    <div className="md:w-3/5 md:pt-24 pb-10 lg:py-32 xl:py-48">
                         {user?
-                            <h1 class="text-3xl lg:text-3xl xl:text-5xl font-black text-gray-900 text-center md:text-left tracking-tighter f-f-i md:w-7/12 leading-tight text-heading-color mb-6">Hi {user?.displayName}</h1>:''
+                            <h1 className="text-3xl lg:text-3xl xl:text-5xl font-black text-gray-900 text-center md:text-left tracking-tighter f-f-i md:w-7/12 leading-tight text-heading-color mb-6">Hi {user?.displayName}</h1>:''
                         }
-                        <h1 class="text-3xl lg:text-3xl xl:text-5xl font-black text-gray-900 text-center md:text-left tracking-tighter f-f-i md:w-7/12 leading-tight text-heading-color">Welcome To Dashboard</h1>
-                        <h2 class="md:w-8/12 my-4 text-center md:text-left md:my-8 text-gray-700 text-lg lg:text-2xl">Do you want to control your expenses and be always aware of how much money you have spent?</h2>
+                        <h1 className="text-3xl lg:text-3xl xl:text-5xl font-black text-gray-900 text-center md:text-left tracking-tighter f-f-i md:w-7/12 leading-tight text-heading-color">Welcome To Dashboard</h1>
+                        <h2 className="md:w-8/12 my-4 text-center md:text-left md:my-8 text-gray-700 text-lg lg:text-2xl">Do you want to control your expenses and be always aware of how much money you have spent?</h2>
                     </div>
-                    <div class="w-1/2 sm:w-2/5 h-64 md:h-auto m-auto flex items-center overflow-hidden">
-                        <img class="md:absolute md:w-1/2 md:-ml-28" src="https://i.ibb.co/yWt9TRB/dashboard.png" alt="DashboardImage"/>
+                    <div className="w-1/2 sm:w-2/5 h-64 md:h-auto m-auto flex items-center overflow-hidden">
+                        <img className="md:absolute md:w-1/2 md:-ml-28" src="https://i.ibb.co/yWt9TRB/dashboard.png" alt="DashboardImage"/>
                     </div>
                 </div>
             </div>
--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -2,9 +2,205 @@ import React from 'react';
 
 const Service = () => {
     return (
-        <div>
-            <h2>Our All Services</h2>
-        </div>
+        <>
+            <div className="xl:mx-auto xl:container  xl:px-20 md:px-6 px-4 py-12">
+                <div className="lg:flex items-center justify-center lg:space-x-12 2xl:space-x-6">
+                    <div className="text-left">
+                        <p className="lg:text-4xl text-3xl font-extrabold leading-9 text-gray-800">Why choose us</p>
+                        <p className="text-lg leading-7 text-gray-600 mt-4 xl:w-7/12 w-full">Whether article spirits new her covered hastily sitting her. Money witty books nor son add</p>
+                        <div className="lg:hidden lg:w-3/5 xl:w-3/5 w-full lg:mt-0 mt-6">
+                            <img src="https://i.ibb.co/SKLJ7WX/austin-distel-jp-Hw8ndw-J-Q-unsplash-1.png" alt="ongoing meeting" className="w-full obejct-fit object-center object-fill h-full" />
+                        </div>
+                        <div className="mt-6 md:mt-10 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6 lg:mt-6 2xl:mt-12">
+                            <div className="flex items-center">
+                                <div className="w-16 h-16 relative">
+                                    <div className=" flex-shrink-0 z-20  w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_6_Svg1.svg" alt="clock"/>
+                                    </div>
+                                </div>
+                                <div className="flex items-start flex-col ml-6 pt-8">
+                                    <h2 className="text-lg font-semibold leading-4 text-gray-800">Time Prompt</h2>
+                                    <p className="lg:w-40 2xl:w-52 text-base leading-6 mt-2 text-gray-600">Whether article spirits new her covered.</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center">
+                                <div className="w-16 h-16 relative">
+                                    <div className=" flex-shrink-0 z-20  w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_6_Svg2.svg" alt="Friendly"/>
+                                    </div>
+                                </div>
+                                <div className="flex items-start flex-col ml-6 pt-8">
+                                    <h2 className="text-lg font-semibold leading-4 text-gray-800">Customer Friendly</h2>
+                                    <p className="lg:w-40 2xl:w-52 text-base leading-6 mt-2 text-gray-600">Whether article spirits new her covered.</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center">
+                                <div className="w-16 h-16 relative">
+                                    <div className=" flex-shrink-0 z-20  w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_6_Svg3.svg" alt="Creative"/>
+                                    </div>
+                                </div>
+                                <div className="flex items-start flex-col ml-6 pt-8">
+                                    <h2 className="text-lg font-semibold leading-4 text-gray-800">Creative Ideas</h2>
+                                    <p className="lg:w-40 2xl:w-52 text-base leading-6 mt-2 text-gray-600">Whether article spirits new her covered.</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center">
+                                <div className="w-16 h-16 relative">
+                                    <div className=" flex-shrink-0 z-20  w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_6_Svg4.svg" alt="Achievments"/>
+                                    </div>
+                                </div>
+                                <div className="flex items-start flex-col ml-6 pt-8">
+                                    <h2 className="text-lg font-semibold leading-4 text-gray-800">Achievments</h2>
+                                    <p className="lg:w-40 2xl:w-52 text-base leading-6 mt-2 text-gray-600">Whether article spirits new her covered.</p>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                    <div className="hidden lg:block lg:w-3/5 xl:w-3/5 w-full lg:mt-0 mt-6">
+                        <img src="https://i.ibb.co/SKLJ7WX/austin-distel-jp-Hw8ndw-J-Q-unsplash-1.png" alt="ongoing meeting" className="w-full obejct-fit object-center object-fill h-full" />
+                    </div>
+                </div>
+            </div>
+           
+            <section className="max-w-8xl mx-auto container bg-white pt-6">
+                    <div>
+                        <div className="flex items-center flex-col px-4">
+                            <p className="focus:outline-none uppercase text-sm text-center text-gray-600 leading-4">in few easy steps</p>
+                            <h1 className="focus:outline-none text-4xl lg:text-4xl font-extrabold text-center leading-10 text-gray-800 lg:w-5/12 md:w-9/12 pt-4">Our Best Features are</h1>
+                        </div>
+                        <div aria-label="group of cards" className="focus:outline-none mt-20 flex flex-wrap justify-center gap-10 px-4 text-left">
+                            <div aria-label="card 1" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                                <div className="w-20 h-20 relative mr-5">
+                                    <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1"></div>
+                                    <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG1.svg" alt="drawer"/>
+                                    </div>
+                                </div>
+                                <div className="w-10/12">
+                                    <h2 className="focus:outline-none text-lg font-bold leading-tight text-gray-800">Ready to use components</h2>
+                                    <p className="focus:outline-none text-base text-gray-600 leading-normal pt-2">It provides a very simple start, no need to write a lot of code, you just import it and start the primitive components and create the ones you need.</p>
+                                </div>
+                            </div>
+                            <div aria-label="card 2" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                                <div className="w-20 h-20 relative mr-5">
+                                    <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1"></div>
+                                    <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG2.svg" alt="check"/>
+                                    </div>
+                                </div>
+                                <div className="w-10/12">
+                                    <h2 className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">Hight Quality UI you can reply on</h2>
+                                    <p className="focus:outline-none text-base text-gray-600 leading-normal pt-2">Modify the visual appearance of your site – including colors, fonts, margins and other style-related properties – with a sophisticated style.</p>
+                                </div>
+                            </div>
+                            <div aria-label="card 3" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                                <div className="w-20 h-20 relative mr-5">
+                                    <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1"></div>
+                                    <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG3.svg" alt="html tag"/>
+                                    </div>
+                                </div>
+                                <div className="w-10/12">
+                                    <h2 className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">Coded by Developers for Developers</h2>
+                                    <p className="focus:outline-none text-base text-gray-600 leading-normal pt-2">Instead of just giving you the tools to create your own site, they offer you a list of themes you can choose from. Thus a handy product.</p>
+                                </div>
+                            </div>
+                            <div aria-label="card 4" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                                <div className="w-20 h-20 relative mr-5">
+                                    <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1"></div>
+                                    <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG4.svg" alt="monitor"/>
+                                    </div>
+                                </div>
+                                <div className="w-10/12">
+                                    <h2 className="focus:outline-none text-lg font-semibold leading-tight text-gray-800">The Last UI kit you’ll ever need</h2>
+                                    <p className="focus:outline-none text-base text-gray-600 leading-normal pt-2">We have chosen the bright color palettes that arouse the only positive emotions. The kit that simply assures to be loved by everyone.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                
+                <div className="dark:bg-gray-900">
+                <section className="mx-auto container py-20">
+                    <div className="flex justify-center items-center flex-col">
+                    <div className="lg:text-6xl md:text-5xl text-4xl font-black leading-10 text-center text-gray-800 dark:text-white">
+                        <h1>Top Features are</h1>
+                    </div>
+                    <div className="pt-24 grid lg:grid-cols-3 md:grid-cols-2 justify-center items-center xl:gap-y-16 gap-y-20 gap-x-16 lg:gap-x-20 xl:gap-x-0 lg:px-10 xl:px-0">
+                        <div className="cursor-pointer hover:shadow py-6 xl:px-4 rounded xl:w-96 w-60 flex justify-center items-center flex-col">
+                        <div className="mb-6">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_7-svg1.svg" alt="arrow-1"/>
+                        </div> 
+                        <div className="text-gray-800 dark:text-white text-2xl font-semibold text-center">
+                            <h2>Robust workflow</h2>
+                        </div>
+                        <div className="text-gray-600 dark:text-gray-300 mt-2 text-lg text-center">
+                            <p>Workflows can help streamline and automate repeatable business tasks.</p>
+                        </div>
+                        </div>
+                        <div className="cursor-pointer hover:shadow py-6 xl:px-4 rounded xl:w-96 w-60 flex justify-center items-center flex-col">
+                        <div className="mb-6">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_7-svg2.svg" alt="3 bars"/>
+                        </div>
+                        <div className="text-gray-800 dark:text-white text-2xl font-semibold text-center">
+                            <h2>Flexibility</h2>
+                        </div>
+                        <div className="text-gray-600 dark:text-gray-300 mt-2 text-lg text-center">
+                            <p>A flexible software architecture is able to changes in usability requirements</p>
+                        </div>
+                        </div>
+                        <div className="cursor-pointer hover:shadow py-6 xl:px-4 rounded xl:w-96 w-60 flex justify-center items-center flex-col">
+                        <div className="mb-6">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_7-svg3.svg" alt="bars"/>
+                        </div>
+                        <div className="text-gray-800 dark:text-white text-2xl font-semibold text-center">
+                            <h2>User friendly</h2>
+                        </div>
+                        <div className="text-gray-600 dark:text-gray-300 mt-2 text-lg text-center">
+                            <p>Good usability can improve a new software chances of successful adoption.</p>
+                        </div>
+                        </div>
+                        <div className="cursor-pointer hover:shadow py-6 xl:px-4 rounded xl:w-96 w-60 flex justify-center items-center flex-col">
+                        <div className="mb-6">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_7-svg4.svg" alt="bars"/>
+                        </div>
+                        <div className="text-gray-800 dark:text-white text-2xl font-semibold text-center">
+                            <h2>Multiple layouts</h2>
+                        </div>
+                        <div className="text-gray-600 dark:text-gray-300 mt-2 text-lg text-center">
+                            <p>Smaller components are easier to maintain. Program can be divided</p>
+                        </div>
+                        </div>
+                        <div className="cursor-pointer hover:shadow py-6 xl:px-4 rounded xl:w-96 w-60 flex justify-center items-center flex-col">
+                        <div className="mb-6">
+                            <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_7-svg5.svg" alt="bars"/>
+                        </div>
+                        <div className="text-gray-800 dark:text-white text-2xl font-semibold text-center">
+                            <h2>Better components</h2>
+                        </div>
+                        <div className="text-gray-600 dark:text-gray-300 mt-2 text-lg text-center">
+                            <p>Component-based design makes front-end web development faster.</p>
+                        </div>
+                        </div>
+                        <div className="cursor-pointer hover:shadow py-6 xl:px-4 rounded xl:w-96 w-60 flex justify-center items-center flex-col">
+                        <div className="mb-6">
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_7-svg6.svg" alt="xdfd"/>
+                        </div>
+                        <div className="text-gray-800 dark:text-white text-2xl font-semibold text-center">
+                            <h2>Well organised</h2>
+                        </div>
+                        <div className="text-gray-600 dark:text-gray-300 mt-2 text-lg text-center">
+                            <p>No more running around looking for things.When you write things and plan.</p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </section>
+            </div>
+        </>
     );
 };
 
